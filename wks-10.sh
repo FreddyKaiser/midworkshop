d8de46d609eacf3702e9b0c2f0bbdd96894fa11b
Freddy Kaiser <freddy.kaiser@swisscom.com>
2013-11-08 16:21:49 +0100

    Radius request

--- wks-10.sh
+++ wks-10.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #
 # Workshop script for radius client calls
+# <radiusport> <msisdn> <optionalPassword>
 
-echo "User-Name=+41798440457" | radclient -t 120 localhost auth TestingRadius@cartel.ch
-echo "User-Name=+41798440457,User-Password=''" | radclient -t 120 localhost auth TestingRadius@cartel.ch
+echo "User-Name=$2,User-Password='$3'" | radclient -t 120 178.209.52.189:$1 auth thisMustStaySecret
 
 #==========================================================
